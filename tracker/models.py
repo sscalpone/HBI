471d33cc1e2bf82e91370bfea3a09dc20b0ac43f
Steve Scalpone <sscalpone@gmail.com>
2014-01-26 20:03:15 -0800

    Keep tabs on these tabs.

--- tracker/models.py
+++ tracker/models.py
@@ -56,11 +56,11 @@ class Child(models.Model):
     def __unicode__(self):
         return self.first_name1
 
-    def age():
-        return age_in_years(birthdate, datetime.datetime.utcnow())
+    def age(self):
+        return self.age_in_years(self.birthdate, datetime.datetime.utcnow())
 
-    def age_in_years(from_date, to_date, leap_day_anniversary_Feb28=True):
-        age = to_date.year - from_date.year
+    def age_in_years(self, from_date, to_date, leap_day_anniversary_Feb28=True):
+        computed_age = to_date.year - from_date.year
         try:
             anniversary = from_date.replace(year=to_date.year)
         except ValueError:
@@ -69,9 +69,9 @@ class Child(models.Model):
                 anniversary = datetime.date(to_date.year, 2, 28)
             else:
                 anniversary = datetime.date(to_date.year, 3, 1)
-        if to_date < anniversary:
-            age -= 1
-        return age
+            if to_date < anniversary:
+                computed_age -= 1
+        return computed_age
 
 class BasicInfo(models.Model):
     child = models.ForeignKey(Child)
