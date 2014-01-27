865ae2eaad5d71f21f2fc73c0b544d829259417a
Steve Scalpone <sscalpone@gmail.com>
2014-01-26 16:33:26 -0800

    Added evaluation model.

--- tracker/models.py
+++ tracker/models.py
@@ -46,9 +46,9 @@ class Child(models.Model):
     first_name2 = models.CharField(max_length=200)
     last_name1 = models.CharField(max_length=200)
     last_name2 = models.CharField(max_length=200)
-    birthdate = models.DateField();
-    birthplace = models.CharField(max_length=200);
-    intake_date = models.DateField();
+    birthdate = models.DateField()
+    birthplace = models.CharField(max_length=200)
+    intake_date = models.DateField()
 
     def __unicode__(self):
         return self.name
@@ -56,6 +56,41 @@ class Child(models.Model):
 class BasicInfo(models.Model):
     child = models.ForeignKey(Child)
 
-class ExamInfo(models.Model):
+class SocialExamInfo(models.Model):
     child = models.ForeignKey(Child)
+    date = models.DateField()
+    has_birth_certificate = models.BooleanField()
+    original_birth_certificate = models.BooleanField()
+    dni = models.BooleanField()
+    dni_in_process = models.BooleanField()
+    dni_comments = models.TextField()
+    sis = models.BooleanField()
+    sis_in_process = models.BooleanField()
+    sis_comments = models.TextField()
+    antecedents = models.TextField()
+    family_situation = models.TextField()
+    health_situation = models.TextField()
+    housing_situation = models.TextField()
+    enconomic_situation = models.TextField()
+    general_comments = models.TextField()
+    visitors_allowed = models.BooleanField()
+    visitors_notes = models.TextField()
+    social_diagnosis = models.TextField()
+    recommendation = models.TextField()
+
+class SocialExamInfo(models.Model):
+    child = models.ForeignKey(Child)
+    date = models.DateField()
+    family_notes = models.TextField()
+    physical_description = models.TextField()
+    intelectual_notes = models.TextField()
+    organicity_notes = models.TextField()
+    psychomotor_notes = models.TextField()
+    emotional_notes = models.TextField()
+    recommendation = models.TextField()
+
+class SocialExamDiagnosis(models.Model):
+    title = models.CharField(max_length=200)
+    dwmq = models.CharField(max_length=200)
+    exam = models.ForeignKey(SocialExamInfo)
 
