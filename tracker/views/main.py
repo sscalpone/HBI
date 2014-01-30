d3876954a36327050eddf488b8056b66834f9942
Steve Scalpone <sscalpone@gmail.com>
2014-01-29 23:14:36 -0800

    Add simple-mindex login page.

--- tracker/views/main.py
+++ tracker/views/main.py
@@ -2,6 +2,8 @@ from django.http import Http404
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext, loader
@@ -10,4 +12,21 @@ def index(request):
     return render(request, 'tracker/index.html')
 
 def login(request):
-    return HttpResponseRedirect(reverse('tracker:main'))
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse('tracker:children'))
+        else:
+            # Return a 'disabled account' error message
+            return render(request, 'tracker/index.html', {
+                'error_message': "You are not an active user.",
+            })
+    else:
+        # Return an 'invalid login' error message
+        return render(request, 'tracker/index.html', {
+            'error_message': "Your username and password do not match.",
+        })
+
