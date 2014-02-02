5847923acf4c25591346aca5295299febcb39a46
J A Burrows <jill@adaburrows.com>
2014-02-01 16:38:17 -0800

    Adding Spanish Labels for ChildForm

--- tracker/models/ChildForm.py
+++ tracker/models/ChildForm.py
@@ -1,11 +1,8 @@
+# coding=utf-8
+
 import datetime
-
 from Child import Child
-
 from django.db import models
-
-from django.contrib.auth.models import User
-
 from django.forms import ModelForm
 
 
@@ -13,4 +10,13 @@ class ChildForm(ModelForm):
     class Meta:
         model = Child
         fields = '__all__'
+        labels = {
+            'residence': 'Casa Girasoles',
+            'first_name': 'Nombres',
+            'last_name': 'Apellidos',
+            'birthdate': 'Fecha de Naciemiento',
+            'birthplace': 'Lugar de Naciemiento',
+            'intake_date': 'Fecha de Ingreso',
+            'photo': 'Fotografía',
+        }
 
