ff0fb8fdd1609474b6ff31231bac4eebf7b75353
Steve Scalpone <sscalpone@gmail.com>
2015-07-15 13:10:42 -0700

    Update to Python 1.7.

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,6 +134,8 @@ INSTALLED_APPS = (
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
