8d3c5dfdee3504490962f1c685a044381c556c40
Gina Scalpone <ginascalpone@gmail.com>
2015-07-24 18:56:54 -0700

    Set up dental exam history to display on general information page, by modifying dental_exam view index function to display to multiple templates and modify urls.py to render view from dental_exam.

---
+++ tracker/views/exam_views.py
@@ -0,0 +1,8 @@
+# from views import main
+# from views import residence
+from views import child
+from views import dental_exam
+from views import medical_exam_part1
+from views import medical_exam_part2
+from views import psychological_exam
+from views import social_exam
