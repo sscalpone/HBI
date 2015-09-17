1540fa0a4452e8f502ec2617aea585c250dbd116
Gina Scalpone <ginascalpone@gmail.com>
2015-09-17 15:14:57 -0700

    Fix readme and reset.sh so me.png shows up.

--- reset.sh
+++ reset.sh
@@ -4,5 +4,6 @@ rm -f database/mysite.db
 python manage.py syncdb --noinput
 
 python manage.py loaddata fixtures/admin_data.json 
+cp fixtures/me.png media/photos/
 python manage.py loaddata fixtures/basic.json 
 
