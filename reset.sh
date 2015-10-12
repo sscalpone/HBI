3ade256df7cae3357baa8e316da6f1813b1eb9a0
Gina Scalpone <ginascalpone@gmail.com>
2015-10-12 16:53:02 -0700

    Fix bugs in documents, add remove statements to reset.sh

--- reset.sh
+++ reset.sh
@@ -3,7 +3,9 @@ rm -f database/mysite.db
 
 python manage.py syncdb --noinput
 
-python manage.py loaddata fixtures/admin_data.json 
+rm media/photos/*
+rm media/documents/*
+python manage.py loaddata fixtures/admin_data.json
 cp fixtures/me.png media/photos/
 python manage.py loaddata fixtures/basic.json 
 
