256b754f32b0ab303b7e36c73074a8a3c8544b7a
Gina Scalpone <ginascalpone@gmail.com>
2016-07-03 13:20:56 -0700

    Register CustomUser with admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from tracker.models import Child
 from tracker.models import Residence
+from tracker.models import CustomUser
 
 admin.site.register(Child)
 admin.site.register(Residence)
+admin.site.register(CustomUser)
